be1a91922ab612c62cfe43af6d4421ba78587a8a
LeeSunR <sd844@naver.com>
2018-10-11 13:57:21 +0900

    index.html [HOME slide]

---
+++ js/cart.js
@@ -0,0 +1,15 @@
+function addCart(){
+  alert("ADDCART");
+}
+
+function delCart(){
+  alert("DELCART");
+}
+
+function order(){
+  alert("order");
+}
+
+function emptyCart(){
+  alert("emptyCart");
+}
